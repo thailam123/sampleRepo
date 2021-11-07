d8dcf72b65979226e7972cb6a9bc1a63a96068b4
thailam123 <52620440+thailam123@users.noreply.github.com>
2021-11-07 21:02:31 +0700

    Create helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Hello, World!"); fsdfd
         System.out.println("Hello, World!"); 
-        System.out.println("Hello, World!"); 
-fdsf
     }
 }
