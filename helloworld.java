2235d36b15bf9123d896fb0e330610fee87ac23d
thaivanlam <thaivanlam171@gmail.com>
2021-11-07 21:06:57 +0700

    sua file

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("Hello, World!"); 
-fdsf
+        System.out.println("Hello, World!"); fdfdsf
     }
 }
