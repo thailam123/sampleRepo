60449cd4b07cf96e3a32043713a8d96d9aa5a177
thailam123 <52620440+thailam123@users.noreply.github.com>
2021-11-07 21:06:23 +0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); fsdfd
+        System.out.println("Hello, World!"); fdfdfd
         System.out.println("Hello, World!"); 
     }
 }
