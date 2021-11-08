b6a9d560250216598bfde928e0ed96413c4e3843
thaivanlam <thaivanlam171@gmail.com>
2021-11-08 19:04:57 +0700

    add thailam1

--- helloworld3.java
+++ helloworld3.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("thaivanlam!"); 
         System.out.println("thaivanlam!"); 
+        System.out.println("thaivanlam!"); 
     }
 }
